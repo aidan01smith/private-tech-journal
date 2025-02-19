1be179dc9170e29713d4f86d946dc829e842af2e
aidan01smith <aidan.smith01@mymail.champlain.edu>
2025-02-19 13:35:38 -0500

    added new ssh script

---
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,8 @@
+# secure-ssh.sh
+# author aidan S
+# creates a new ssj user using $1 parameter
+# adds a public key from he local repo or curles from the remote repo
+# removes roots ability to ssh in
+
+echo "ALL YOUR CODE GOES HERE"
+
