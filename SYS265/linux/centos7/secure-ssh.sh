f308729bb6c1e47a985606c83c8db5a8e42dcf4e
aidan01smith <aidan.smith01@mymail.champlain.edu>
2025-02-20 20:31:03 +0000

    script update

--- SYS265/linux/centos7/secure-ssh.sh
+++ SYS265/linux/centos7/secure-ssh.sh
@@ -1,8 +1,26 @@
+#!/bin/bash
+
 # secure-ssh.sh
 # author aidan S
-# creates a new ssj user using $1 parameter
+# creates a new ssh user using $1 parameter
 # adds a public key from he local repo or curles from the remote repo
 # removes roots ability to ssh in
 
-echo "ALL YOUR CODE GOES HERE"
+sudo useradd -m -d /home/${1} -s /bin/bash ${1}
+sudo mkdir /home/${1}/.ssh
+cd /home/aidan/private-tech-journal
+sudo cp SYS265/linux/public-keys/id_rsa.pub /home/${1}/.ssh/authorized_keys
+sudo chmod 700 /home/${1}/.ssh
+sudo chmod 600 /home/${1}/.ssh/authorized_keys
+sudo chown -R ${1}:${1} /home/${1}/.ssh
 
+# block the root ssh login
+
+if grep -q "^PermitRootLogin" /etc/ssh/sshd_config; then
+	sudo sed -i 's/^PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
+else
+	echo "permit root login not found in /etc/ssh/sshd_config"
+fi
+
+
+sudo systemctl restart sshd.service
